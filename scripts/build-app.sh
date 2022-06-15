40de0562f8c3719f28130cb1d7485d31bf957cef
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-06-15 15:07:41 +0200

    remove wildcard and be explisit

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -2,7 +2,7 @@
 pushd /template/Tjololo.DI.Template
 
 pushd /
-dotnet add /template/Tjololo.DI.Template/Tjololo.DI.Template.csproj reference /customize/**/*.csproj || true
+dotnet add /template/Tjololo.DI.Template/Tjololo.DI.Template.csproj reference /customize/Tjololo.DI.Customize/Tjololo.DI.Customize.csproj || true
 popd
 
 dotnet build Tjololo.DI.Template.csproj --configuration Release --output /app/build
